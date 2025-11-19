cce6c09d3a8eabfb2e9ac6a54abcc48b24ddcb29
Silvère MARIE <silvere.marie@decathlon.com>
2025-11-19 15:30:08 +0100

    add user AVSC generation test and utility method

--- src/test/java/com/decathlon/finance/avrodemo/AvroDemoApplicationTests.java
+++ src/test/java/com/decathlon/finance/avrodemo/AvroDemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.decathlon.finance.avrodemo;
 
+import com.decathlon.finance.avrodemo.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 @SpringBootTest
 class AvroDemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void generateUserAvsc() throws IOException {
+        generateAvsc(User.SCHEMA$.toString(), "target/avsc/user.avsc");
     }
+
+
+    private Path generateAvsc(String formatted, String path) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter writer = objectMapper.writerWithDefaultPrettyPrinter();
+        return Files.write(Paths.get(path), writer.writeValueAsString(objectMapper.readTree(formatted)).getBytes(), StandardOpenOption.CREATE);
+    }
+
+
 
 }
---
+++ src/test/java/com/decathlon/finance/avrodemo/GenerationTest.java
@@ -0,0 +1,12 @@
+package com.decathlon.finance.avrodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class GenerationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
